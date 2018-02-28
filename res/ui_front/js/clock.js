027406cad7fae27bafe8b1ed54fea21c6a61ac7c
Rich Village <RicheVille703@gmail.com>
2018-02-28 17:59:43 +0900

    디자인 안을 따라 만든 데스크탑 UI 입니다 | Desktop UI based on design

---
+++ res/ui_front/js/clock.js
@@ -0,0 +1,38 @@
+﻿function startTime () {
+	var today = new Date();
+	var AMPM = null;
+	var year = today.getFullYear();
+	var month = today.getMonth() + 1;
+	var date = today.getDate();
+	var hour = today.getHours();
+	var minute = today.getMinutes();
+	var second = today.getSeconds();
+
+	if (hour > 12) {
+		AMPM = "오후";
+		hour = hour - 12;
+	}
+
+	else if (hour == 12) {
+		AMPM = "오후";
+	}
+
+	else {
+		AMPM = "오전";
+	}
+
+	minute = checkTime(minute);
+	second = checkTime(second);
+	
+	document.getElementById('clock').innerHTML = AMPM + " " + hour + ":" + minute;
+	document.getElementById('date').innerHTML = year + "-" + month + "-" + date;
+	var t = setTimeout(startTime, 500);
+}
+
+function checkTime (i) {
+	if (i < 10) {
+		// 숫자가 0보다 작으면 앞에 0을 붙여준다
+		i = "0" + i;
+	}
+	return i;
+}
